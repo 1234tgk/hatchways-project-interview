fabe67f4cc5d86942f59e564ba9070cd194e0fd4
Taehyeon Kim <1234tgk@gmail.com>
2021-07-15 13:37:46 -0400

    combine ifs on api/messages

--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -17,13 +17,12 @@ router.post("/", async (req, res, next) => {
     // do this if conversation has been found
     if (conversation) {
       const { user1Id, user2Id } = conversation.dataValues;
-      // if user1Id does not match senderId nor recipientId, throw unauthorized
-      if (user1Id !== senderId && user1Id !== recipientId) {
-        return res.sendStatus(401);
-      }
-      // same for user2Id
-      if (user2Id !== senderId && user2Id !== recipientId) {
-        return res.sendStatus(401);
+      // if user1Id or user2Id does not match senderId nor recipientId, throw unauthorized
+      if (
+        (user1Id !== senderId && user1Id !== recipientId) ||
+        (user2Id !== senderId && user2Id !== recipientId)
+      ) {
+        return res.sendStatus(403);
       }
       // add the message since there is no problem
       const message = await Message.create({ senderId, text, conversationId });
@@ -32,11 +31,8 @@ router.post("/", async (req, res, next) => {
 
     if (!conversation) {
       // if sender is null, we have prohibited request
-      if (!sender) {
-        return res.sendStatus(403);
-      }
       // if sender.id does not match with senderId, send 403
-      if (sender.id !== senderId) {
+      if (!sender || sender.id !== senderId) {
         return res.sendStatus(403);
       }
       // create conversation
